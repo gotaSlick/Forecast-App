575f9ede49b9b7a74d01007e98ae7624ae2ce629
Gota <30895814+gotaSlick@users.noreply.github.com>
2018-01-21 19:16:40 +0100

    Update GoogleMap.js

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
-    //first create a render method and return a single element (div) and give it a prop called ref 
+    //first create a render method and return a single element (div), then give it a prop called ref 
     //and assign it a string of "map". This makes use of the ref (reference) system in react. A direct
     //reference to a html element that has been rendered to the page. So afer this component has been 
     //rendered to the screen I can get a direct reference to the div created right here by refering to 
